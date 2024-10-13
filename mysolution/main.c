7fbd9cef2a6bdc3cfde38db53192b4b759a3b128
z-sn <z.sn.rust@gmail.com>
2024-10-12 18:30:45 -0700

    Initial commit for Nucleo-F429ZI programming

---
+++ mysolution/main.c
@@ -0,0 +1,23 @@
+// Copyright (c) 2022 Cesanta Software Limited
+// All rights reserved
+
+int main(void) {
+  return 0;
+}
+
+// Startup code
+__attribute__((naked, noreturn)) void _reset(void) {
+  // memset .bss to zero, and copy .data section to RAM region
+  extern long _sbss, _ebss, _sdata, _edata, _sidata;
+  for (long *dst = &_sbss; dst < &_ebss; dst++) *dst = 0;
+  for (long *dst = &_sdata, *src = &_sidata; dst < &_edata;) *dst++ = *src++;
+
+  main();             // Call main()
+  for (;;) (void) 0;  // Infinite loop in the case if main() returns
+}
+
+extern void _estack(void);  // Defined in link.ld
+
+// 16 standard and 91 STM32-specific handlers
+__attribute__((section(".vectors"))) void (*const tab[16 + 91])(void) = {
+    _estack, _reset};
