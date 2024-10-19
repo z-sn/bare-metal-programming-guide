1e52747ecf5414bccd404fa6998ca4179bb5bcf9
z-sn <z.sn.rust@gmail.com>
2024-10-19 16:04:18 -0700

    Added mysolution_CMSIS

---
+++ mysolution_CMSIS/main.c
@@ -0,0 +1,42 @@
+#include "hal.h"
+
+
+static volatile uint32_t s_ticks;
+void SysTick_Handler(void) {
+  s_ticks++;
+}
+
+void SystemInit(void) {
+
+}
+
+/* This logic works because when the later (s_ticks) rolls over, its value will be close
+ * to UNINT_MAX as 2^32 will be added to it, making the value positive number.
+ */
+static uint32_t elapsed_time(uint32_t later, uint32_t start){
+  return later - start;
+}
+
+int main(void) {
+  uint16_t led = PIN('B', 7);             // Blue LED
+  RCC->AHB1ENR |= BIT(PINBANK(led));      // Enable GPIO clock for LED
+  systick_init(16000000 / 1000);
+  gpio_set_mode(led, GPIO_MODE_OUTPUT);
+  uart_init(UART3, 115200);               // Initialise UART
+  uint32_t start = s_ticks;
+  bool on = true;         // This block is executed
+  int i = 0;
+  for (;;) {
+    // 200ms timer
+    if (elapsed_time(s_ticks, start) > 500) {
+      gpio_write(led, on);    // Every 'period' milliseconds
+      on = !on;
+      start = s_ticks;
+      //uart_write_buf(UART3, "hi\r\n", 4);
+      printf("%d  : LED : %d, tick: %lu\r\n",i, on, s_ticks);
+      i++;
+    }
+  }
+  return 0;
+}
+
---
+++ mysolution_CMSIS/startup.c
@@ -0,0 +1,20 @@
+// Startup code
+__attribute__((naked, noreturn)) void _reset(void) {
+  // memset .bss to zero, and copy .data section to RAM region
+  extern long _sbss, _ebss, _sdata, _edata, _sidata;
+  for (long *dst = &_sbss; dst < &_ebss; dst++) *dst = 0;
+  for (long *dst = &_sdata, *src = &_sidata; dst < &_edata;) *dst++ = *src++;
+
+  // Call main()
+  extern void main();
+  main();
+  for (;;) (void) 0;  // Infinite loop in the case if main() returns
+}
+
+extern void SysTick_Handler(void);  // Define in main.c
+extern void _estack(void);          // Defined in link.ld
+
+// 16 standard and 91 STM32-specific handlers
+__attribute__((section(".vectors"))) void (*const tab[16 + 91])(void) = {
+    _estack, _reset, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, SysTick_Handler};
+
